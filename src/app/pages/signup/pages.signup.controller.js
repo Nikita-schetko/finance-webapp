abb603aeb1fe39e5f1c9b3a04309f931fda17cf7
Nikita-schetko <corrosion.by@gmail.com>
2017-05-28 23:03:18 +0300

    Some validations to signup form added

--- src/app/pages/signup/pages.signup.controller.js
+++ src/app/pages/signup/pages.signup.controller.js
@@ -12,18 +12,9 @@
 
     vm.showToastr = showToastr;
 
-    // activate();
-
-    // function activate() {
-    //   $timeout(function() {
-    //     // vm.classAnimation = 'rubberBand';
-    //     vm.classAnimation = 'rubberBand';
-    //   }, 4000);
-    // }
-
     function showToastr() {
       toastr.info('There should be a AJAX call to backend API or at least POST request...');
-      vm.classAnimation = '';
+      vm.classAnimation = ''; 
     }
   }
 })();
