51dd43aa1f0038248d9eee3c1d3896122fbeea4b
Nikita-schetko <nschetko@Evoke.by>
2017-05-29 17:34:52 +0300

    signup form validation changes

--- src/app/pages/signup/pages.signup.controller.js
+++ src/app/pages/signup/pages.signup.controller.js
@@ -9,10 +9,21 @@
   function SignupController(toastr) {
     var vm = this;
 
+    vm.CheckPassword = function(password, confirmPassword)
+    {
+        if(password == confirmPassword)
+        {
+            vm.passwordEquals = true;
+            vm.passwordClass = "passwordMatch";
+        }
+        else
+        {
+            vm.passwordEquals = false;
+            vm.passwordClass = "";
+        }
+    }
 
-    vm.showToastr = showToastr;
-
-    function showToastr() {
+    vm.showToastr = function () {
       toastr.info('There should be a AJAX call to backend API or at least POST request...');
       vm.classAnimation = ''; 
     }
