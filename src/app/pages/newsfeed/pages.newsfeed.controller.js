080cfd507603c29ab48247ad549f07c50bcee3ab
Nikita-schetko <nschetko@Evoke.by>
2017-06-09 10:17:33 +0300

    Newsfeed: shareidea functionality; images added to newsfeed;

--- src/app/pages/newsfeed/pages.newsfeed.controller.js
+++ src/app/pages/newsfeed/pages.newsfeed.controller.js
@@ -8,23 +8,34 @@
   /** @ngInject */
   function NewsfeedController() {
     var vm = this;
+    vm.postsId = 4;
     vm.share = function() 
     {
+      if(vm.postMessage)
+      {
         vm.posts.push({
+            id: vm.postsId,
             userAvatar: "assets/images/person64.png",
             message: vm.postMessage
-        });   
-        vm.postMessage = '';   
+        });
+        vm.postsId++;   
+        vm.postMessage = ''; 
+      }
     }
     vm.posts = [{
+     id:1,
      userAvatar:"assets/images/user1.png",
      message:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit itaque consectetur repellat autem, dolorum sit voluptate laboriosam natus dicta provident!"
     },{
+      id:2,
       userAvatar:"assets/images/user2.png",
-      message:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora labore quasi architecto rem dolorum nam eum id dolores, ut, consectetur veniam! Reprehenderit enim quos tenetur officia rerum veniam totam fugit inventore recusandae, quasi beatae, similique excepturi voluptatibus corporis laborum, maxime magni est itaque cumque ratione."
+      message:"U.K. Prime Minister Theresa May's future as Britain's top politician was in doubt Friday after polls suggest her decision to call a snap election has narrowed the already-slim majority for her ruling Conservative party",
+      image:"assets/images/tweetImages/stream_85440989-.png"
     },{
+      id:3,
       userAvatar:"assets/images/user3.png",
-      message:"Lorem ipsum dolor sit amet, consectetur adipisicing elit.Labore maxime quia eos quas debitis eum repellat perferendis placeat, dolor. Obcaecati explicabo at eius ipsum eaque ex vero minima dignissimos, reiciendis consequuntur ut, corporis?"
+      message:"German Dep ForMin: Can’t Waste Any Time Negotiating Brexit - RTRS",
+      image:"assets/images/tweetImages/stream_85461306-.jpg"
     }];
   }
 })();
